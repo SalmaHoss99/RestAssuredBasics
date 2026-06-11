911bde43a4fb680e66e335b8d06d8662c9ae0c27
salma <salmahossam639@gmail.com>
2026-06-12 00:30:16 +0300

    users api testing

--- src/test/java/tests/UsersApiTest.java
+++ src/test/java/tests/UsersApiTest.java
@@ -1,4 +1,24 @@
 package tests;
 
-public class UserApiTest {
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UsersApiTest{
+
+    @Test
+    public void getSingleUserShouldReturnStatusCode200() {
+
+        RestAssured.baseURI = "https://reqres.in";
+
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .when()
+                .get("/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2));
+    }
 }
